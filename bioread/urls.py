00234fde20277ef0c36302818fae37bd4c09e8e4
huechingkao <huechingkao@gmail.com>
2018-07-28 00:27:26 -0400

    first commit

---
+++ bioread/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  
+    path('', TemplateView.as_view(template_name="homepage.html")),  
+    path('account/', include('account.urls')),  
+] 
